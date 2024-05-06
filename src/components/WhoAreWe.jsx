a0406d51883e340e17d1847f1d77d09eb578ae87
AashnaShimkhada <55328807+AashnaShimkhada@users.noreply.github.com>
2024-05-06 13:31:16 -0700

    Update WhoAreWe.jsx

--- src/components/WhoAreWe.jsx
+++ src/components/WhoAreWe.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import App from "./App";
+import './WhoAreWe.css';
 
 const WhoAreWe = () => {
   return (
-    <div>WhoAreWe</div>
+    <div className='container'>
+      <div className='text-section'>
+        <h1>
+          Who We Are
+        </h1>
+        <p1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum. Quam vel aliquam sit vulputate. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum. Quam vel aliquam sit vulputate. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum. Quam vel aliquam sit vulputate. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum. Quam vel aliquam sit vulputate. 
+        </p1>
+        <button className='button'>
+          Meet the Girls Who Code Team!
+        </button>
+        </div>
+        <div className='image-section'>
+          <img src='gwc-logo.png'/>
+      </div>
+      
+    </div>
   )
 }
 
